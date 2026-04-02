09a5700e392b3616e16ad40c201881d9f38d01bc
lkno9 <ghost.yami09@gmail.com>
2026-04-02 20:32:58 +0200

    feat: remettre Mes avantages dans le menu principal

--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -30,6 +30,7 @@ const PRIMARY_SERVICES = [
   { icon: <Wrench className="w-6 h-6" />, label: "Outils pratiques", sublabel: "Météo, minuteur, calcul", path: "/services/tools" },
   { icon: <Cloud className="w-6 h-6" />, label: "Mes documents", sublabel: "Courriers, ordonnances", path: "/services/storage" },
   { icon: <Gamepad2 className="w-6 h-6" />, label: "Jeux & mémoire", sublabel: "Mémoire, quiz, sudoku", path: "/services/games" },
+  { icon: <Sparkles className="w-6 h-6" />, label: "Mes avantages", sublabel: "Bons plans seniors", path: "/services/partners" },
   { icon: <ShieldAlert className="w-6 h-6" />, label: "Ma sécurité", sublabel: "Arnaques, alertes", path: "/services/scam-protection" },
   { icon: <Newspaper className="w-6 h-6" />, label: "Actualités & infos", sublabel: "L'info du jour", path: "/services/knowledge" },
 ];
